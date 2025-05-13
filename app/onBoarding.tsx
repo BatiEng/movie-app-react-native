c761e96c48da01ad663df330a0bafc337143d308
BatiEng <alibatikan@gmail.com>
2025-05-14 00:03:35 +0300

    first

---
+++ app/onBoarding.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const onBoarding = () => {
+  return (
+    <View>
+      <Text>onBoarding</Text>
+    </View>
+  );
+};
+
+export default onBoarding;
+
+const styles = StyleSheet.create({});
